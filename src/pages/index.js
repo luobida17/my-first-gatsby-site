e8fc2c295fe588ff6cb2c9cedde03de8d27f0fda
luobida17 <1637208761@qq.com>
2021-07-06 09:59:03 -0700

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 // import { Link } from 'gatsby'
 
 //my component
@@ -15,6 +16,7 @@ const IndexPage = () => {
         // </main>
         <Layout pageTitle="Home Page">
             <p>I'm making this by following the Gatsby Tutorial.</p>
+            <StaticImage alt="moon" src="https://images.unsplash.com/photo-1620118733503-ab4e1cb6025d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1789&q=80"/>
         </Layout>
     )
 
